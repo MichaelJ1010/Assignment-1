b61b2cee0fa9c2dec3f2b5f6a8fc522d7636696a
Michael J <30031129@cc.peralta.edu>
2024-09-03 15:34:41 -0700

    updated cout to be cleaner

--- class.cpp
+++ class.cpp
@@ -3,11 +3,11 @@ int main(){
     int num1 = 2;
     int num2 = 1;
     int temp;
-    std :: cout << "hello\n" << num1 << " " << num2;
+    std :: cout << num1 << " " << num2;
     temp = num1;
     num1 = num2;
     num2 = temp;
-    std :: cout << "hello\n" << num1 << " " << num2;
+    std :: cout << num1 << " " << num2;
 
     return 0;
 }
