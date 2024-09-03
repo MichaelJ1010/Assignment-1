64d2c538daba6a4421a769288e00723f54d876a1
Michael J <30031129@cc.peralta.edu>
2024-09-03 15:41:27 -0700

    added new message when printing

--- class.cpp
+++ class.cpp
@@ -3,10 +3,11 @@ int main(){
     int num1 = 2;
     int num2 = 1;
     int temp;
-    std :: cout << num1 << " " << num2;
+    std :: cout << num1 << " " << num2<< "\n";
+    std :: cout << "swapping num1 and num2..." << "\n";
     temp = num1;
     num1 = num2;
-    num2 = temp;
+    num2 = temp; 
     std :: cout << num1 << " " << num2;
 
     return 0;
