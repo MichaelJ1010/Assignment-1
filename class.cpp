d36339a4d07fb6bc05c70e9aea7141d9a4211d3a
Michael J <30031129@cc.peralta.edu>
2024-09-03 15:25:10 -0700

    class.cpp

---
+++ class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+    int num1 = 2;
+    int num2 = 1;
+    int temp;
+    std :: cout << "hello\n" << num1 << " " << num2;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    std :: cout << "hello\n" << num1 << " " << num2;
+
+    return 0;
+}
